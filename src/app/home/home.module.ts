c28d2eead27e2d985e4e4dfc56a335f14c66105c
thayronFeitosa <thayronf3@gmail.com>
2022-03-22 16:42:59 -0300

    Criando componente de login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,18 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ],
-  exports: [
-    HomeComponent
-  ]
+  declarations: [HomeComponent, LoginComponent],
+  imports: [CommonModule, HomeRoutingModule],
+  exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
