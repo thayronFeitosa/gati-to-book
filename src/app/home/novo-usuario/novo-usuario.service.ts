a5fc8c1382b4139f98945d54ea833626dc100728
thayronFeitosa <thayronf3@gmail.com>
2022-03-25 06:10:02 -0300

    criando o formulario e usando o formulario reatico angular

---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NovoUsuario } from './novo-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class NovoUsuarioService {
+
+  constructor(private http: HttpClient) { }
+
+  create(novoUsario: NovoUsuario) {
+    return this.http.post('http://localhost:3000/user/signup',novoUsario )
+  }
+}
