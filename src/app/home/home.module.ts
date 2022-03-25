91e238eeec6caf4a0a157dfb9dff15c55a3b8c86
thayronFeitosa <thayronf3@gmail.com>
2022-03-25 08:32:49 -0300

    Criando validaçao no formulario

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit  } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { NovoUsuario } from './novo-usuario';
 
@@ -13,14 +13,23 @@ export class NovoUsuarioComponent implements OnInit {
   novoUsuarioForm!:FormGroup;
 
   constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService) { }
-
+  
   ngOnInit(): void {
     
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
-      userName: [''],
-      password: [''],
+      email: ['', [
+        Validators.required, Validators.email
+      ]],
+      fullName: ['', [
+        Validators.required, Validators.minLength(4)
+      ]],
+      userName: ['', [
+        Validators.required, Validators.minLength(4)
+
+      ]],
+      password: ['', [
+        Validators.required, Validators.minLength(4)
+      ]],
     })
   }
   cadatrar() {
